d005040a431a494bb0f7826d93528643cfd7be62
RISHABH SHARMA <rishabhsharma@Rishabhs-MacBook-Pro.local>
2017-12-05 01:12:33 -0500

    project completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'welcome/index'
+  resources :notes
+
+  authenticated :user do
+  	root 'notes#index', as: "authenticated_root"
+  end
 
   root "welcome#index"
 end
